450a6a17bb5cc8a192dad669913aef1fd1af9a8f
minky <150100967+kwakminkyeong@users.noreply.github.com>
2024-09-19 15:16:39 +0900

    Add files via upload

---
+++ Study02/src/test/Sample.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Sample {
+	public static void main(String[] args) {//프로그램의 실행 시작점
+		System.out.println("다음의 내용을 출력");//컴퓨터로부터 한줄로 출력하라 내용시작 ;내용끝.
+		System.out.println("이것도 출력"); 
+		//주석은 다음과 같이 남길 수 있습니다.
+		//주석부분은 컴퓨터가 인식할 수 없습니다. 컴퓨터와 소통하는 언어를 배웁니다.
+		/*여러줄은 이렇게 주석처리가 가능합니다. 주석으로 협업 소통한다. 소스코드는 누가봐도 이해할 수 있는 코드이다.*/
+	}//시작점의 종료부분
+} //Sample설계도 종료부분
