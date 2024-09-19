2ccdbff3b15a76d39a799246447609546d6caf42
minky <150100967+kwakminkyeong@users.noreply.github.com>
2024-09-19 15:15:49 +0900

    Add files via upload

---
+++ Study01/src/Start/Hello.java
@@ -0,0 +1,15 @@
+package Start;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("한글도 되는군요");
+		System.out.println("여러줄 넣어도 됩니다");
+		System.out.println("팩키지 스타트를 확인한다");
+		System.out.println("공개된 클래스를 확인한다");
+		System.out.println("A.B A안에 있는 B");
+		System.out.println("{} 조각조각내서 나눠준다");
+		System.out.println("<h1>안녕하세요 자바! </h1>");
+		
+	}
+}
+
