07aa53d34b49265beaa95697a5f872973da8db6d
minky <150100967+kwakminkyeong@users.noreply.github.com>
2024-09-19 15:19:15 +0900

    Add files via upload

---
+++ Study04/src/name/Name.java
@@ -0,0 +1,22 @@
+package name;
+
+public class Name {
+	public static void main(String[] args) {
+		//변수 작명기법
+		//1.이름은  대소문자를 엄격하게 구별한다.
+		//2.이름에는 띄어쓰기를 사용할 수 없다.
+		//3.특수문자는 _ $ 두가지만 사용할 수 있다.
+		//4.이름은 숫자로 시작할 수 없다.
+		//int a; //변수를 만드는 것도 시작점 안에 있어야한다.
+		
+		int iot_programming;
+		iot_programming=25;
+		System.out.println(iot_programming);
+		
+		//작명시 참조할 수 있는 관습
+		//파스칼표기 : JavaHtmlCss 첫번째 글자를 대문자로 구별되어 있는 단어.클래스 이름 지을때 라이브러리 이름 지을때 많이 사용된다.
+		//카멜표기 : javaHtmlCss, 두번째단어부터의 첫글자를 대문자로 구별.method이름지을때 사용
+		//팟홀표기 : java_html_css, 변수이름지을때 사용
+	}
+
+}
