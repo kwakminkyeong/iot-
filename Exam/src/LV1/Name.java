19f7a13ea951575d2bff7a92ea5c9a5ab6c3da7b
minky <150100967+kwakminkyeong@users.noreply.github.com>
2024-09-19 15:40:08 +0900

    Add files via upload

---
+++ Exam/src/LV1/Name.java
@@ -0,0 +1,22 @@
+package LV1;
+
+public class Name {
+	public static void main(String[] args) {
+		//다음 소스코드에서 틀린 부분을 확인하고 수정한 후 주석으로 이유를 남기시오.
+		
+		int a2=10;
+		int b3=150;//숫자만 들어가야하는데 " " 문자
+		int c4=75; //변수명은 숫자가 먼저 나오면 안됨
+		boolean a=true; //변수상자를 만들어줘야한다
+		String e="테스트"; //" "쌍따옴표 적용 
+		float f=16.5f; //뒤에 f로 표시 변수타입을 double f=16.5;
+		a2=100; //데이터를 변경해준다
+		int b5=90; //변수상자 중복됨
+		boolean test_data=false; //변수상자 이름에 공백이 있으면 안됨.팟홀표기법, 변수이름
+		boolean testData=false; //카멜표기법, 메서드(동작)이름
+		boolean TestData=false; //파스칼표기법, 클래스이름
+		
+		//다른방향으로도 생각해본다.
+	}
+
+}
